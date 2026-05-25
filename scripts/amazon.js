ef84b2d5dc91a0b658e6d3a97239845ef4c90f94
solkuzzzz <solkzkreztiv@mail.ru>
2026-05-25 18:48:30 +0300

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   rating: {
-      stars: 4.5,
-      count: 87
-   },
-   priceCents: 1090
-}, {
-   image: 'images/products/intermediate-composite-basketball.jpg',
-   name: 'Intermediate Size Basketball',
-   rating: {
-      stars: 4,
-      count: 127
-   },
-   priceCents: 2095
-}, {
-   image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-   name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-   rating: {
-      stars: 4.5,
-      count: 56
-   },
-   priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -39,14 +13,14 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
          <img class="product-rating-stars"
-            src="images/ratings/${product.rating.stars * 10}.png">
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
                ${product.rating.count}
             </div>
       </div>
 
       <div class="product-price">
-         ${(product.priceCents / 100).toFixed(2)}
+         $${(product.priceCents/100).toFixed(2)}
       </div>
 
       <div class="product-quantity-container">
@@ -76,3 +50,5 @@ products.forEach((product) => {
       </button>
    </div>`
 })
+
+document.querySelector('.products-grid').innerHTML = productsHTML;
